4508556ed0453d2aec0e804f2661d32122a59e0e
Chris Gianelloni <wolf31o2@blinklabs.io>
2023-11-12 12:26:56 -0500

    chore: update license headers
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,9 @@
+// Copyright 2023 Blink Labs Software
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 package config
 
 import (
