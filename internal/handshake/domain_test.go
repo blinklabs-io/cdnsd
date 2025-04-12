e8d2e407ffe170e4e0ebcd06bf27b50d3200f7ee
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-04-12 09:22:43 -0400

    chore: make format golines
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- internal/handshake/domain_test.go
+++ internal/handshake/domain_test.go
@@ -16,7 +16,9 @@ import (
 
 func TestDomainResourceData(t *testing.T) {
 	// This data comes from mainnet TX 63ba84b6362724aa8fd484d3616c8d1bdea68240c8e0cd6a104fcf85a35d52fb
-	testResourceDataBytes := decodeHex("0002036e73310a69727677696c6c69616d002ce706b701c00202036e7332c00636d688f601c01a00d5580d0114402ed0125506f35ba249265f39b988d7028a28c300d5580d02200c6c45064c26b529b4ac074dff5de60a99d6025d5b0d7f32c2b8c7d40ec8b3de00d5580d043071cb0417852b08b965413f3b871b033996159d121a585e35111a335d4cfb79b67e49a99c3829f6a1f42e100f7f33d7d9")
+	testResourceDataBytes := decodeHex(
+		"0002036e73310a69727677696c6c69616d002ce706b701c00202036e7332c00636d688f601c01a00d5580d0114402ed0125506f35ba249265f39b988d7028a28c300d5580d02200c6c45064c26b529b4ac074dff5de60a99d6025d5b0d7f32c2b8c7d40ec8b3de00d5580d043071cb0417852b08b965413f3b871b033996159d121a585e35111a335d4cfb79b67e49a99c3829f6a1f42e100f7f33d7d9",
+	)
 	expectedResourceData := &handshake.DomainResourceData{
 		Version: 0,
 		Records: []handshake.DomainRecord{
@@ -38,23 +40,31 @@ func TestDomainResourceData(t *testing.T) {
 				KeyTag:     54616,
 				Algorithm:  13,
 				DigestType: 1,
-				Digest:     decodeHex("402ed0125506f35ba249265f39b988d7028a28c3"),
+				Digest: decodeHex(
+					"402ed0125506f35ba249265f39b988d7028a28c3",
+				),
 			},
 			&handshake.DsDomainRecord{
 				KeyTag:     54616,
 				Algorithm:  13,
 				DigestType: 2,
-				Digest:     decodeHex("0c6c45064c26b529b4ac074dff5de60a99d6025d5b0d7f32c2b8c7d40ec8b3de"),
+				Digest: decodeHex(
+					"0c6c45064c26b529b4ac074dff5de60a99d6025d5b0d7f32c2b8c7d40ec8b3de",
+				),
 			},
 			&handshake.DsDomainRecord{
 				KeyTag:     54616,
 				Algorithm:  13,
 				DigestType: 4,
-				Digest:     decodeHex("71cb0417852b08b965413f3b871b033996159d121a585e35111a335d4cfb79b67e49a99c3829f6a1f42e100f7f33d7d9"),
+				Digest: decodeHex(
+					"71cb0417852b08b965413f3b871b033996159d121a585e35111a335d4cfb79b67e49a99c3829f6a1f42e100f7f33d7d9",
+				),
 			},
 		},
 	}
-	tmpResourceData, err := handshake.NewDomainResourceDataFromBytes(testResourceDataBytes)
+	tmpResourceData, err := handshake.NewDomainResourceDataFromBytes(
+		testResourceDataBytes,
+	)
 	if err != nil {
 		t.Fatalf("unexpected error decoding resource data: %s", err)
 	}
